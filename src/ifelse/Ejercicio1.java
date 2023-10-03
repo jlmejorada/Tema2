de183c21470382f0a0c418af73e986d31d1e5455
jlmejorada <jlmejorada@106-18.info2>
2023-10-03 13:43:11 +0200

    Subida ejercicio 1

--- src/ifelse/Ejercicio1.java
+++ src/ifelse/Ejercicio1.java
@@ -1,10 +1,33 @@
 package ifelse;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Creamos una variable para almacenar el número que pidamos y otra para el modulo
+		int num, mod;
+		
+		//Creamos la variable escaner
+		Scanner sc = new Scanner(System.in);
+		
+		//Pedimos el número y lo recogemos
+		System.out.println("Dame un número");
+		num = sc.nextInt();
+		
+		//Calculamos el modulo del número entre 2, para ver si es par o impar
+		mod = num % 2;
+		
+		//Creamos una condicional que pille el modulo de la division pase un mensaje si es par o impar
+		if (mod==0) {
+			System.out.println("EL número es par");
+		} else {
+			System.out.println("El número es impar");
+		}
+		
+		//Cerramos el escaner
+		sc.close();
 	}
+
 
 }
