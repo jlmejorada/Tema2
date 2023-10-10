452988d09f899d8012fbe9cc6a49cd8c2ade3f1d
jlmejorada <joseluis.mejorada@iesnervion.es>
2023-10-10 13:13:38 +0200

    Subida ejercicio

---
+++ src/switchej/Ejercicio1.java
@@ -0,0 +1,45 @@
+package switchej;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// Creamos una variable para la nota entera
+		int num;
+		
+		//Creamos la variable escaner
+		Scanner sc = new Scanner(System.in);
+		
+		//Pedimos el numero entero y lo recogemos
+		System.out.println("Dame el número entero de tu nota");
+		num= sc.nextInt();
+		
+		switch (num) {
+		case 0, 1, 2, 3, 4 -> {
+			System.out.println("INSUFICIENTE");
+			System.out.println("Tienes que estudiar más");
+		}
+		case 5 -> {
+			System.out.println("SUFICIENTE");
+		}
+		case 6 -> {
+			System.out.println("BIEN");
+		}
+		case 7, 8 -> {
+			System.out.println("NOTABLE");
+		}
+		case 9, 10 -> {
+			System.out.println("SOBRESALIENTE");
+		}
+		default -> {
+			System.out.println("Esa nota no es posible");
+		}
+		}
+		
+		//Cerramos el escaner
+		sc.close();
+		
+	}
+
+}
