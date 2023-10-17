9d4be958e461f8a0315db4b9195be26de3e2a903
jlmejorada <joseluis.mejorada@iesnervion.es>
2023-10-17 13:59:33 +0200

    subida ej5

---
+++ src/whileej/Ejercicio5.java
@@ -0,0 +1,38 @@
+package whileej;
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		// Creamos una variable que ira recogiendo números
+		int num, sum=0, med=0, cont=0, neg=0, i=1;
+		
+		//Creamos la variable escaner 
+		Scanner sc = new Scanner(System.in);
+		
+		while (i<=10) {
+			
+			//Pedimos un número y lo recogemos
+			System.out.println("Dame un número");
+			num = sc.nextInt();
+			
+			if (num>0) {
+				sum+=num;
+			} else if (num<0) {
+				med+=num;
+				neg++;
+			} else cont++;
+			
+			
+			i++;
+		}
+		//calculamos la media
+		med=med/neg;
+		
+		System.out.println("La suma de los positivos da " + sum + ", la media de los negativos es " + med + " y has introducido " + cont + " ceros.");
+
+		sc.close();
+	}
+
+}
