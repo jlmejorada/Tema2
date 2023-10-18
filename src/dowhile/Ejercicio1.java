6efb604978b320f44c6ed6329d72b61a4a80adc1
jlmejorada <joseluis.mejorada@iesnervion.es>
2023-10-18 10:23:18 +0200

    subida ej1

---
+++ src/dowhile/Ejercicio1.java
@@ -0,0 +1,17 @@
+package dowhile;
+
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// Creamos las variables necesarias
+		int cont=1;
+		
+		do {
+			System.out.println(cont);
+			cont++;
+		} while (cont<=20);
+
+	}
+
+}
