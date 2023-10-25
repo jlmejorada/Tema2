45998e35a137c4767285049e91c5eb72dbfa5575
jlmejorada <joseluis.mejorada@iesnervion.es>
2023-10-25 08:37:48 +0200

    subida ej3

---
+++ src/bluclefor/Ejercicio3.java
@@ -0,0 +1,18 @@
+package bluclefor;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		
+		// Un bucle for que muestre los multiplos de 7, menor que 100
+		for (int i = 0 ; i<=100 ; i++ ) {
+			// Este if comprueba si un número es multiplo de 7
+			if (i%7 == 0) {
+				System.out.println(i);
+			}
+			
+		}
+
+	}
+
+}
