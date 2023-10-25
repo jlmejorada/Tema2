7c479edbfcd298defc55f818c182e323a0ba8984
jlmejorada <joseluis.mejorada@iesnervion.es>
2023-10-25 08:37:36 +0200

    subida ej2

---
+++ src/bluclefor/Ejercicio2.java
@@ -0,0 +1,13 @@
+package bluclefor;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		
+		// Un bucle for que muestre los resultados pares del 2 al 200
+		for (int i = 2 ; i<=200 ; i+=2 ) 
+			System.out.println(i);
+
+	}
+
+}
